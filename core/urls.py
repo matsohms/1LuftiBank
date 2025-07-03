bef3dbea9c3fef2bc85226ab846d844afb921beb
matsohms <matsohms@icloud.com>
2025-07-03 18:12:06 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import login_view, admin_dashboard
-
+from .views import (
+    login_view, admin_home, admin_dashboard,
+    customer_list, customer_create, customer_detail
+)
 urlpatterns = [
-    path('', login_view, name='login'),
-    path('login/', login_view),
-    path('admin-dashboard/', admin_dashboard, name='admin-dashboard'),
+    path('',              login_view,       name='login'),
+    path('login/',        login_view),
+    path('admin/',        admin_home,       name='admin_home'),
+    path('admin/dashboard/', admin_dashboard, name='admin_dashboard'),
+    path('admin/customers/', customer_list,   name='customer_list'),
+    path('admin/customers/add/', customer_create, name='customer_create'),
+    path('admin/customers/<int:pk>/', customer_detail, name='customer_detail'),
 ]
