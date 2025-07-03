6707445460112ea1edabb9964eb8101729aaaf72
matsohms <matsohms@icloud.com>
2025-07-03 19:06:29 +0200

    Update wsgi.py

--- banking_portal/wsgi.py
+++ banking_portal/wsgi.py
@@ -1,5 +1,17 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
+# ENV-Settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'banking_portal.settings')
+
+# WSGI-App erstellen
 application = get_wsgi_application()
+
+# Automatisch Datenbank-Migrationen ausführen (Session- & Core-Model-Tabellen)
+try:
+    from django.core.management import call_command
+    call_command('makemigrations', '--noinput')
+    call_command('migrate', '--noinput')
+except Exception:
+    # Fehler ignorieren, falls schon migriert oder im Production nicht gewünscht
+    pass
