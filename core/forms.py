287d36b4f6feaabd2666419710c33e5a3db7a1df
matsohms <matsohms@icloud.com>
2025-07-03 18:10:20 +0200

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,19 @@
 from django import forms
+from .models import Customer
 
 class LoginForm(forms.Form):
     account_number = forms.CharField(label='Kontonummer', max_length=8)
     pin            = forms.CharField(label='PIN', widget=forms.PasswordInput)
     totp_code      = forms.CharField(label='TOTP-Code', max_length=6)
+
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = [
+            'first_name','last_name','middle_names',
+            'address_line1','address_line2','postal_code','city',
+            'birth_date','phone','email'
+        ]
+        widgets = {
+            'birth_date': forms.DateInput(attrs={'type':'date'})
+        }
