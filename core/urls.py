ef897f0dd127bcc7b68421f21d31cb856313e952
matsohms <matsohms@icloud.com>
2025-07-03 17:35:27 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,6 @@ from django.urls import path
 from .views import login_view, admin_dashboard
 
 urlpatterns = [
-    # Root und /login/ zeigen das Login-Formular
-    path('',      login_view,      name='login'),
+    path('', login_view, name='login'),
     path('login/', login_view),
-
-    # Dashboard nur nach Login
     path('admin-dashboard/', admin_dashboard, name='admin-dashboard'),
-]
