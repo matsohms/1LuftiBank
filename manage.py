eae8e5cb6a6fa8728f7e38c3cc402997cfc56e4a
matsohms <matsohms@icloud.com>
2025-07-03 17:08:12 +0200

    Create manage.py

---
+++ manage.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import os, sys
+from dotenv import load_dotenv
+load_dotenv()
+
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'banking_portal.settings')
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
